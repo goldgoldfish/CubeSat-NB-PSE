a536dfe950b4dff6c9f2be0148f1dfdcc1a97bc4
Chris Thomas <cthom1080@gmail.com>
2019-03-17 16:48:59 -0300

    Revert "fixing merge conflict"
    This reverts commit 3574df0fb5a30ae446d90fa343fa70b4d5535cee.

--- Atmel Studio/7.0/MEGA_UART_INTERRUPT_EXAMPLE1/MEGA_GPIO_EXAMPLE1/Debug/src/adc_func.d
+++ Atmel Studio/7.0/MEGA_UART_INTERRUPT_EXAMPLE1/MEGA_GPIO_EXAMPLE1/Debug/src/adc_func.d
@@ -1,65 +1,62 @@
-src/adc_func.d src/adc_func.o: ../src/adc_func.c ../src/adc_func.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- ../src/ASF/mega/utils/compiler.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom640.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iomxx0_1.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- ../src/ASF/common/utils/parts.h ../src/ASF/common/utils/interrupt.h \
- ../src/ASF/common/utils/interrupt/interrupt_avr8.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- ../src/ASF/mega/utils/progmem.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
- ../src/config/conf_example.h ../src/adc.h ../src/adc_megaxx0_1_4.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\float.h
+src/adc_func.d src/adc_func.o src/adc_func.o: ../src/adc_func.c \
+  ../src/adc_func.h c:/winavr/lib/gcc/../../avr/include/stdint.h \
+  ../src/ASF/mega/utils/compiler.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr/lib/gcc/../../avr/include/inttypes.h \
+  C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom640.h \
+  C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iomxx0_1.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/lock.h \
+  c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stdbool.h \
+  c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr/lib/gcc/../../avr/include/stdlib.h \
+  ../src/ASF/common/utils/parts.h ../src/ASF/common/utils/interrupt.h \
+  ../src/ASF/common/utils/interrupt/interrupt_avr8.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/interrupt.h \
+  ../src/ASF/mega/utils/progmem.h \
+  c:/winavr/lib/gcc/../../avr/include/avr/pgmspace.h \
+  ../src/config/conf_example.h ../src/adc.h ../src/adc_megaxx0_1_4.h \
+  c:/winavr/lib/gcc/../../avr/include/string.h \
+  c:/winavr/lib/gcc/../../avr/include/math.h \
+  c:/winavr/lib/gcc/../../avr/include/stdio.h \
+  c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:\winavr\bin\../lib/gcc/avr/4.3.3/include/float.h
 
 ../src/adc_func.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+c:/winavr/lib/gcc/../../avr/include/stdint.h:
 
 ../src/ASF/mega/utils/compiler.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+c:/winavr/lib/gcc/../../avr/include/avr/io.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+c:/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+c:/winavr/lib/gcc/../../avr/include/inttypes.h:
 
 C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom640.h:
 
 C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iomxx0_1.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+c:/winavr/lib/gcc/../../avr/include/avr/portpins.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+c:/winavr/lib/gcc/../../avr/include/avr/common.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+c:/winavr/lib/gcc/../../avr/include/avr/version.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+c:/winavr/lib/gcc/../../avr/include/avr/fuse.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+c:/winavr/lib/gcc/../../avr/include/avr/lock.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
+c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stdbool.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+c:/winavr/lib/gcc/../../avr/include/stdlib.h:
 
 ../src/ASF/common/utils/parts.h:
 
@@ -67,11 +64,11 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 ../src/ASF/common/utils/interrupt/interrupt_avr8.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+c:/winavr/lib/gcc/../../avr/include/avr/interrupt.h:
 
 ../src/ASF/mega/utils/progmem.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+c:/winavr/lib/gcc/../../avr/include/avr/pgmspace.h:
 
 ../src/config/conf_example.h:
 
@@ -79,12 +76,12 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 ../src/adc_megaxx0_1_4.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+c:/winavr/lib/gcc/../../avr/include/string.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+c:/winavr/lib/gcc/../../avr/include/math.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+c:/winavr/lib/gcc/../../avr/include/stdio.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+c:\winavr\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\float.h:
+c:\winavr\bin\../lib/gcc/avr/4.3.3/include/float.h:
--- Atmel Studio/7.0/MEGA_UART_INTERRUPT_EXAMPLE1/MEGA_GPIO_EXAMPLE1/src/gpio_func.c
+++ Atmel Studio/7.0/MEGA_UART_INTERRUPT_EXAMPLE1/MEGA_GPIO_EXAMPLE1/src/gpio_func.c
@@ -31,27 +31,27 @@ void Set_GPIO(int gpio_num){
 	// PORT H
 	if (gpio_num < 7){
 		gpio = gpio << gpio_num; // shift to correct bit
-		PORTH |= gpio; // set selected pin high
+		PORTH = gpio; // set selected pin high
 	}
 	// PORT L
 	else if (gpio_num >= 7 && gpio_num < 10){
 		gpio = gpio << (gpio_num - 6); // shift to correct bit
-		PORTL |= gpio; // set selected pin high
+		PORTL = gpio; // set selected pin high
 	}
 	// PORT A
 	else if(gpio_num >= 10 && gpio_num < 14){
 		gpio = gpio << (gpio_num - 6); // shift to correct bit
-		PORTA |= gpio; // set selected pin high
+		PORTA = gpio; // set selected pin high
 	}
 	// PORT J
 	else if(gpio_num >= 14 && gpio_num < 21){
 		gpio = gpio << (gpio_num - 14); // shift to correct bit
-		PORTJ |= gpio; // set selected pin high
+		PORTJ = gpio; // set selected pin high
 	}
 	// PORT C
 	else if (gpio_num >= 21 && gpio_num < 29){
 		gpio = gpio << (gpio_num - 21); // shift to correct bit
-		PORTC |= gpio; // set selected pin high
+		PORTC = gpio; // set selected pin high
 	}
 	// Otherwise do nothing
 	else{
@@ -72,32 +72,31 @@ void Set_GPIO(int gpio_num){
 void Clear_GPIO(int gpio_num){
 	
 	int gpio = 0x01; // initialize gpio selection to bit 0
-	int gpio_mask = 0xFF; //mask for XOR
 	
 	// PORT H
 	if (gpio_num < 7){
 		gpio = gpio << gpio_num; // shift to correct bit
-		PORTH &= (gpio^gpio_mask); // use mask to clear bit
+		PORTH &= (!gpio); // use mask to clear bit
 	}
 	// PORT L
 	else if (gpio_num >= 7 && gpio_num < 10){
 		gpio = gpio << (gpio_num - 6); // shift to correct bit
-		PORTL &= (gpio^gpio_mask); // use mask to clear bit
+		PORTL &= (!gpio); // use mask to clear bit
 	}
 	// PORT A
 	else if(gpio_num >= 10 && gpio_num < 14){
 		gpio = gpio << (gpio_num - 6); // shift to correct bit
-		PORTA &= (gpio^gpio_mask); // use mask to clear bit
+		PORTA &= (!gpio); // use mask to clear bit
 	}
 	// PORT J
 	else if(gpio_num >= 14 && gpio_num < 21){
 		gpio = gpio << (gpio_num - 14); // shift to correct bit
-		PORTJ &= (gpio^gpio_mask); // use mask to clear bit
+		PORTJ &= (!gpio); // use mask to clear bit
 	}
 	// PORT C
 	else if (gpio_num >= 21 && gpio_num < 29){
 		gpio = gpio << (gpio_num - 21); // shift to correct bit
-		PORTC &= (gpio^gpio_mask); // use mask to clear bit
+		PORTC &= (!gpio); // use mask to clear bit
 	}
 	// Otherwise do nothing
 	else{
